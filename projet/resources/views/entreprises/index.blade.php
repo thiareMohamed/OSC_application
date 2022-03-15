7b6d387a0d006750cfe3839b29bc55631e52c23e
thiaremohamed.mt@gmail.com <thiaremohamed.mt@gmail.com>
2022-03-15 11:40:39 +0000

    Add login system

--- projet/resources/views/entreprises/index.blade.php
+++ projet/resources/views/entreprises/index.blade.php
@@ -60,6 +60,7 @@
 
             </tbody>
         </table>
+        
     </main>
 </body>
 </html>
--- projet/routes/web.php
+++ projet/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Pays;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaysController;
 use App\Http\Controllers\EntrepriseController;
@@ -16,18 +15,25 @@ use App\Http\Controllers\EntrepriseController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth'])->name('dashboard');
+    
+    Route::get('/pays', [PaysController::class,'index']);
+
+    Route::get('/entreprise', [EntrepriseController::class,'index']);
+    Route::get('/entreprise/create', [EntrepriseController::class,'create']);
+    Route::post('/entreprise/store', [EntrepriseController::class,'store']);
+    Route::get('/entreprise/delete/{entreprise}', [EntrepriseController::class,'delete'])->name('entreprises.delete');
+    Route::get('/entreprise/{entreprise}', [EntrepriseController::class,'show'])->name('entreprises.show');
+
 });
 
-Route::get('/pays', [PaysController::class,'index']);
 
-Route::get('/entreprise', [EntrepriseController::class,'index']);
-Route::get('/entreprise/create', [EntrepriseController::class,'create']);
-Route::post('/entreprise/store', [EntrepriseController::class,'store']);
-Route::get('/entreprise/delete/{entreprise}', [EntrepriseController::class,'delete'])->name('entreprises.delete');
-Route::get('/entreprise/{entreprise}', [EntrepriseController::class,'show'])->name('entreprises.show');
-
-
-
+require __DIR__.'/auth.php';
 
